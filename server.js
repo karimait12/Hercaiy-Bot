2ba55fe2cf34f0b67d5b2e6f5b3e17d5e253e5ac
karimait12 <karimaityahia98@gmail.com>
2025-03-05 17:26:03 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
-const http = require('http');
+const app = require('./index.js');
+const PORT = process.env.PORT || 3000;
 
-exports.server = () => {
-  const server = http.createServer((req, res) => {
-    console.log("200 NEW REQ IN: " + new Date().toLocaleString());
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, world!\n');
-  });
-
-  server.listen(6800, () => {
-    console.log('Server listening on port 3000');
-  });
-}
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
